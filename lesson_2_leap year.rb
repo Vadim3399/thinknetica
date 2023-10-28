fc50ef57e31c7f9aaa7661797d4e44f7613cd1d1
Vadim3399 <120184058+Vadim3399@users.noreply.github.com>
2023-10-29 01:27:32 +0500

    Update lesson_2_leap year.rb

--- lesson_2_leap year.rb
+++ lesson_2_leap year.rb
@@ -5,17 +5,11 @@ month = gets.chomp.to_i
 print "и год: "
 year = gets.chomp.to_i
 sum = 0
-if year % 400 == 0 || ( year % 4 ==0 && year % 100 != 0)
-  days_in_a_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  for i in 0..month - 2
-    sum += days_in_a_month[i]
-    end
-    sum += day
-else
-  days_in_a_month1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  for i in 0..month - 2
-    sum += days_in_a_month1[i]
-    end
-    sum += day
-end
+days_in_a_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days_in_a_month[1] = 29 if year % 400 == 0 || ( year % 4 ==0 && year % 100 != 0)
+sum = 0
+(0..month - 2).each do |month|
+  sum += days_in_a_month[month]
+  end
+  sum += day
 puts "Порядковый номер даты: #{sum}"
