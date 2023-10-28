386dee2b37799d26e65358ca22119c14afa4ec7f
Vadim3399 <120184058+Vadim3399@users.noreply.github.com>
2023-10-29 01:12:39 +0500

    Update lesson_2_leap year.rb

--- lesson_2_leap year.rb
+++ lesson_2_leap year.rb
@@ -5,14 +5,12 @@ month = gets.chomp.to_i
 print "и год: "
 year = gets.chomp.to_i
 sum = 0
-
 if year % 400 == 0 || ( year % 4 ==0 && year % 100 != 0)
   days_in_a_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   for i in 0..month - 2
     sum += days_in_a_month[i]
     end
     sum += day
-
 else
   days_in_a_month1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   for i in 0..month - 2
