234f262f5d221ab6f84e504ff4e6160f1551f9e3
Vadim3399 <120184058+Vadim3399@users.noreply.github.com>
2023-10-28 22:07:59 +0500

    Add files via upload

---
+++ lesson_2_1.rb
@@ -0,0 +1,27 @@
+# Сделать хеш, содержащий месяцы и количество дней в месяце. 
+# В цикле выводить те месяцы, у которых количество дней ровно 30
+# 1 вариант
+year = {
+    January: 31,
+    February: 28,
+    March: 31, 
+    April: 30,
+    May: 31,
+    June: 30,
+    July: 31,
+    August: 31, 
+    September: 30,
+    October: 31,
+    November: 30,
+    December: 31
+}
+
+months = year.select { |moth, days| days == 30 }
+puts months
+
+# второй вариант
+# month_days = {jan: 31, feb: 28, march: 31, apr: 30, may: 31, jun: 30, jul: 31, aug: 31, sept: 30, oct: 31, nov: 30, dec: 31}
+# month_days.each do |month, days|
+#   puts month if days == 30
+# end
+
