8ba2e91610aece6788a917df02b63e640d75fdc3
Vadim3399 <120184058+Vadim3399@users.noreply.github.com>
2023-10-29 23:51:57 +0500

    Update and rename lesson_2_leap_year.rb to leap_year.rb

--- leap_year.rb
+++ leap_year.rb
@@ -10,6 +10,6 @@ days_in_a_month[1] = 29 if year % 400 == 0 || ( year % 4 ==0 && year % 100 != 0)
 sum = 0
 (0..month - 2).each do |month|
   sum += days_in_a_month[month]
-  end
-  sum += day
+end
+sum += day
 puts "Порядковый номер даты: #{sum}"
